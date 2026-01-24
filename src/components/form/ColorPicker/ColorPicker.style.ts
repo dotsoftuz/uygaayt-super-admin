341f0a744cbc0570416f781fdfe443870a116576
Muhammad Anvarov <mukhammadxuja@gmail.com>
2026-01-24 17:22:35 +0500

    Update ColorPicker and ProductLocationSelector components

--- src/components/form/ColorPicker/ColorPicker.style.ts
+++ src/components/form/ColorPicker/ColorPicker.style.ts
@@ -41,3 +41,4 @@ export const ColorPickerStyled = styled.div`
 
 
 
+
--- src/components/form/ProductLocationSelector/index.ts
+++ src/components/form/ProductLocationSelector/index.ts
@@ -1,3 +1,4 @@
 export { default } from './ProductLocationSelector';
 export type { Section, SelectedLocation } from './ProductLocationSelector';
 
+
